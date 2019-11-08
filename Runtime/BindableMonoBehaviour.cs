4d0668fc4e9309d557260f0a7b601abb5a5f659f
Cory Leach <cory.leach@gmail.com>
2019-11-08 13:43:23 -0500

    Update BindableMonoBehaviour.cs

--- Runtime/BindableMonoBehaviour.cs
+++ Runtime/BindableMonoBehaviour.cs
@@ -17,18 +17,18 @@ namespace Gameframe.Bindings
         /// <summary>
         /// Use this inside property setters to raise property changed events which are needed by bindingssss
         /// </summary>
-        /// <param name="storage">storage location where value will be set</param>
+        /// <param name="field">field to which the value will be set</param>
         /// <param name="value">value you want to assign to storage</param>
         /// <param name="propertyName">name of the property being set</param>
         /// <typeparam name="T">Type of the property being set</typeparam>
         /// <returns>True if property was set. False if it was already equal to the given value.</returns>
-        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null )
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null )
         {
-            if (EqualityComparer<T>.Default.Equals(storage,value))
+            if (EqualityComparer<T>.Default.Equals(field,value))
             {
                 return false;
             }
-            storage = value;
+            field = value;
             OnPropertyChanged(propertyName);
             return true;
         }
