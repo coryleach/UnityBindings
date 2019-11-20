e30681611aea7761b10ea10ec3a4c99b05c8279b
Cory Leach <cory.leach@gmail.com>
2019-11-20 18:30:29 -0500

    Fix warnings.

---
+++ Tests/Runtime/BindingsRuntimeTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Gameframe.Bindings.Tests.Runtime
+{
+    public class BindingsRuntimeTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BindingsRuntimeTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator BindingsRuntimeTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
